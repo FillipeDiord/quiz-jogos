b98d3a7200667cbf08cf8000e713fcebef069bc0
Fillipe Diord <fillipe.diord@hotmail.com>
2023-05-28 10:38:05 -0300

    finish aplicattion

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
-const answersQuestions = [`A`, `A`, `A`, `A`,];
-
-const form = document.querySelector(`form`);
-
-form.addEventListener(`submit`, (event) => {
-  event.preventDefault();
-  let score = 0;
-
-  answersUser = [
-    form.inputQuestion1.value,
-    form.inputQuestion2.value,
-    form.inputQuestion3.value,
-    form.inputQuestion4.value,
-  ];
-
-  answersUser.forEach((response, index) => {
-    if (response === answersQuestions[index]) {
-      score += 25;
-    }
+function handleCheckAnswers() {
+  const answersQuestions = [`C`, `D`, `A`, `B`,];
+  
+  const form = document.querySelector(`form`);
+  
+  form.addEventListener(`submit`, (event) => {
+    event.preventDefault();
+    let score = 0;
+  
+    answersUser = [
+      form.inputQuestion1.value,
+      form.inputQuestion2.value,
+      form.inputQuestion3.value,
+      form.inputQuestion4.value,
+    ];
+  
+    answersUser.forEach((response, index) => {
+      if (response === answersQuestions[index]) {
+        score += 25;
+      }
+    });
+  
+    backToTop();
+    
+    let counterScore = 0;
+  
+    showPanelScore(counterScore, score);
+  
   });
+}
 
-  alert(`Você conseguiu: ${score} pontos`);
+function backToTop() {
+  scrollTo(0, 0);
+}
 
-});
+function showPanelScore(counterScore, score) {
+  const finalResult = document.querySelector(`.result`);
+  
+  finalResult.classList.remove(`d-none`);
+
+  const idInterval = setInterval(() => {
+    if (counterScore === score) {
+      clearInterval(idInterval);
+    }
+
+    finalResult.querySelector(`span`).textContent = `${counterScore}%`;
+
+    counterScore++
+
+  }, 10);
+}
+
+handleCheckAnswers();
